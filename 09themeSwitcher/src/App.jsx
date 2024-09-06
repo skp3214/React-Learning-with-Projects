6dc6a58271806e3da6ffdcc062b2105e4ab77b90
Sachin Prajapati <skprajapati3214@gmail.com>
2024-09-06 10:11:17 +0530

    all about custom Context

---
+++ 09themeSwitcher/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeButton';
+import Card from './components/Card';
+
+const App = () => {
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+  useEffect(() => {
+    document.querySelector('html').classList.remove("ligth", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  return (
+
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+
+  )
+}
+
+export default App
